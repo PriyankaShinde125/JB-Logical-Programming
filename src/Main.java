7e7c7663b82ace3daf7dbeb5612201ce77e0e908
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-21 09:35:09 +0530

    ADDED : Code to find square root

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to logical programming");
+        double epsilon = 1.0E-15;
+        double num;
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextDouble();
+        findSqrt(num, epsilon);
+
+    }
+
+    private static void findSqrt(double c, double epsilon) {
+        double t = c;
+        double root;
+        int count = 0;
+
+        while (true) {
+            count++;
+            root = 0.5 * (t + (c / t));
+            if (Math.abs(root - t) < epsilon)
+                break;
+            t = root;
+        }
+        System.out.println("Square root of " + c + " is " + t);
     }
 }
